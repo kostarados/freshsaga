bb62ffc376545b4199c31af9b99854f9309f1255
kostarados <47539159+kostarados@users.noreply.github.com>
2019-03-07 20:18:51 +0000

    Updates

---
+++ precache-manifest.dec48eb5c29a475b7949d87eb8f9b15c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fc8ad5075bcd40ffede67f98459f1ae",
+    "url": "/freshsaga/static/media/logo.5fc8ad50.png"
+  },
+  {
+    "revision": "332348644d94bb269344ee9839c87ffd",
+    "url": "/freshsaga/static/media/dpad.33234864.png"
+  },
+  {
+    "revision": "06d20e22250f9dcac80b",
+    "url": "/freshsaga/static/js/runtime~main.06d20e22.js"
+  },
+  {
+    "revision": "d9aea012544cbdda95fd",
+    "url": "/freshsaga/static/js/main.d9aea012.chunk.js"
+  },
+  {
+    "revision": "7db0cfb4e8cd7a2cfec1",
+    "url": "/freshsaga/static/js/1.7db0cfb4.chunk.js"
+  },
+  {
+    "revision": "d9aea012544cbdda95fd",
+    "url": "/freshsaga/static/css/main.69f92131.chunk.css"
+  },
+  {
+    "revision": "628b5eee574af0bb7a1856e18c3e5695",
+    "url": "/freshsaga/index.html"
+  }
+];
